d0c6984a2f2bd6ba7d2ba88d62e3175861db2c00
yijaejun <yijaejun81@gmail.com>
2016-03-07 16:04:27 +0900

    commit test with webStorm

--- routes/routes.js
+++ routes/routes.js
@@ -6,14 +6,7 @@ Router.configure({
     layoutTemplate: 'layout'
 });
 
-/**
- * 랜딩 페이지
- * 클립 불러오기
- * TODO todo앱을 클립 불러오는 앱으로 변경해보자
- * offset : 시작지점
- * size : 크기
- */
-Router.route('/clipplr/:offset/:size/list/', function () {
+Router.route('/', function () {
     this.render('todo', {
         data: function () {
             return null;
@@ -22,7 +15,35 @@ Router.route('/clipplr/:offset/:size/list/', function () {
 });
 
 /**
+ * 랜딩 페이지
+ * 클립 불러오기
+ * TODO todo앱을 클립 불러오는 앱으로 변경해보자
+ * offset : 시작지점
+ * size : 크기
+ */
+Router.route('/list/:_size/:_offset', function () {
+    this.render('list', {
+        data: function () {
+            return null;
+        }
+    });
+});
+
+/**
+ * 클립보기
+ * TODO 1. 로그인을 하고 해당 콘텐츠의 소유자일 경우 수정하기 버튼이 노출된다.
+ */
+Router.route('/content/:_id', function () {
+    this.render('content', {
+        data: function () {
+            return null;
+        }
+    });
+});
+
+/**
  * 클립 쓰기
+ * TODO 1. 로그인을 해야 클립쓰기에 접근할 수 있다.
  */
 Router.route('/editor', function () {
     this.render('editor', {
@@ -34,8 +55,10 @@ Router.route('/editor', function () {
 
 /**
  * 클립 수정하기
+ * TODO 1.로그인을 했을 경우에만 수정이 가능하도록 하며,
+ * TODO 2.로그인을 하지 않은 상태로 URL에 접속을 하려고 했을 때 list페이지로 리다이렉트 시킨다
  */
-Router.route('/editor/:id/modify', function () {
+Router.route('/editor/:_id/modify', function () {
     this.render('editor', {
         data: function () {
             return null;
@@ -45,8 +68,9 @@ Router.route('/editor/:id/modify', function () {
 
 /**
  * 클립 지우기
+ * TODO 1.로그인을 하지 않으면 접속할 수 없다
  */
-Router.route('/editor/:id/delete', function () {
+Router.route('/editor/:_id/delete', function () {
     this.render('editor', {
         data: function () {
             return null;
