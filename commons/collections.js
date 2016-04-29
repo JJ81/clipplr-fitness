ce943476da33ac9b3a62aca616b207183baf4f79
yijaejun <yijaejun81@gmail.com>
2016-04-30 07:23:01 +0900

    no message

--- commons/collections.js
+++ commons/collections.js
@@ -3,10 +3,10 @@
  */
 
 // 유저의 운동 기록
-// FitnessRecord = new Meteor.Collection('fitnessRecord');
+FitnessRecord = new Meteor.Collection('fitnessRecord');
 // 운동 등록
 Exercise = new Mongo.Collection('admin_create_exercise');
 // 프로그램, 훈련 등록
-// Programs = new Mongo.Collection('admin_create_program');
+Programs = new Mongo.Collection('admin_create_program');
 
 
